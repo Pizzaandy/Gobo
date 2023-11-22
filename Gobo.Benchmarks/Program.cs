83ab7cf0cd7d182d15a46b598999dbd487c94717
Zandyball <47232037+Pizzaandy@users.noreply.github.com>
2023-11-21 22:59:04 -0600

    benchmark project setup

---
+++ Gobo.Benchmarks/Program.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnostics.Windows.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using Gobo;
+using Gobo.Tests;
+
+namespace CSharpier.Benchmarks;
+
+[EtwProfiler]
+[MemoryDiagnoser]
+[SimpleJob(RuntimeMoniker.NativeAot80)]
+public class Benchmarks
+{
+    private readonly string largeCode = File.ReadAllText(
+        Path.Combine(DirectoryFinder.FindParent("Gobo.Benchmarks").FullName, "BigFile.txt")
+    );
+
+    [Benchmark]
+    public void Default_CodeFormatter()
+    {
+        GmlFormatter.Format(largeCode, FormatOptions.Default);
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        var summary = BenchmarkRunner.Run<Benchmarks>();
+    }
+}
--- Gobo/SyntaxNodes/Gml/Literals/Literal.cs
+++ Gobo/SyntaxNodes/Gml/Literals/Literal.cs
@@ -1,5 +1,4 @@
-﻿using System.Text.RegularExpressions;
-using Gobo.Printer.DocTypes;
+﻿using Gobo.Printer.DocTypes;
 
 namespace Gobo.SyntaxNodes.Gml.Literals;
 
